311e031220d6433b6cf69b3a7baecd93988ae2c3
Ben Li <benji112358@gmail.com>
2018-08-03 19:51:59 -0400

    lambda optim

--- 4_backprop/ex4/nnCostFunction.m
+++ 4_backprop/ex4/nnCostFunction.m
@@ -3,8 +3,18 @@ function [J grad] = nnCostFunction(nn_params, ...
                                    hidden_layer_size, ...
                                    num_labels, ...
                                    X, y, lambda)
+%NNCOSTFUNCTION Implements the neural network cost function for a two layer
+%neural network which performs classification
+%   [J grad] = NNCOSTFUNCTON(nn_params, hidden_layer_size, num_labels, ...
+%   X, y, lambda) computes the cost and gradient of the neural network. The
+%   parameters for the neural network are "unrolled" into the vector
+%   nn_params and need to be converted back into the weight matrices. 
+% 
+%   The returned parameter grad should be a "unrolled" vector of the
+%   partial derivatives of the neural network.
+%
 
-% Reshape nn_params back into the parameters Theta1 and Thet  a2, the weight matrices
+% Reshape nn_params back into the parameters Theta1 and Theta2, the weight matrices
 % for our 2 layer neural network
 Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
                  hidden_layer_size, (input_layer_size + 1));
@@ -21,62 +31,113 @@ Theta1_grad = zeros(size(Theta1));
 Theta2_grad = zeros(size(Theta2));
 
 % ====================== YOUR CODE HERE ======================
-
+% Instructions: You should complete the code by working through the
+%               following parts.
+%
 % Part 1: Feedforward the neural network and return the cost in the
 %         variable J. After implementing Part 1, you can verify that your
 %         cost function computation is correct by verifying the cost
 %         computed in ex4.m
-
-
-% EXTRACT NN-PREDICTIONS......
-
-  % COMPUTING HIDDEN LAYER...
-  a1 = [ones(m,1) X];				% each row of X = training example + bias unit
-  z2 = a1*Theta1';					% input => sigmoid() => hidden label
-
-  % COMPUTING OUTPUT LAYER...
-  a2 = [ones(size(z2,1),1) sigmoid(z2)];		% each row = weighted sum of input-units + bias unit
-  z3 = a2*Theta2';				
-  a3 = sigmoid(z3);					% predictions 
-
- % FORMAT PREDICTION -> VECTOR
- yVec = repmat([1:num_labels], m, 1) == repmat(y, 1, num_labels);
-
- % COST FUNCTION
- J = (sum(sum((-yVec).*log(a3) - (1-yVec).*log(1-a3) )))/m;
- reg = (lambda/(2*m)) * ( sum(sum(Theta1(:,2:end)).^2) + sum(sum(Theta2(:,2:end)).^2)  );
- J = J+ reg; 
- J=J;
-
-
-
+%
 % Part 2: Implement the backpropagation algorithm to compute the gradients
 %         Theta1_grad and Theta2_grad. You should return the partial derivatives of
 %         the cost function with respect to Theta1 and Theta2 in Theta1_grad and
 %         Theta2_grad, respectively. After implementing Part 2, you can check
 %         that your implementation is correct by running checkNNGradients
+%
+%         Note: The vector y passed into the function is a vector of labels
+%               containing values from 1..K. You need to map this vector into a 
+%               binary vector of 1's and 0's to be used with the neural network
+%               cost function.
+%
+%         Hint: We recommend implementing backpropagation using a for-loop
+%               over the training examples if you are implementing it for the 
+%               first time.
+%
+% Part 3: Implement regularization with the cost function and gradients.
+%
+%         Hint: You can implement this around the code for
+%               backpropagation. That is, you can compute the gradients for
+%               the regularization separately and then add them to Theta1_grad
+%               and Theta2_grad from Part 2.
+%
 
 
-% Compute low-deltas
-d3 = a3-yVec;                                                        % error in (Y)
-d2 = (d3*Theta2) .* [ones(size(z2,1),1) sigmoidGradient(z2)];        % error in hidden layer (a2)
+%% Part 1 implementation
+
+a1 = [ones(m, 1) X];
+
+z2 = a1 * Theta1';
+a2 = sigmoid(z2);
+a2 = [ones(size(a2,1), 1) a2];
+
+z3 = a2 * Theta2';
+a3 = sigmoid(z3);
+hThetaX = a3;
+
+yVec = zeros(m,num_labels);
+
+for i = 1:m
+    yVec(i,y(i)) = 1;
+end
+
+% for i = 1:m
+%     
+%     term1 = -yVec(i,:) .* log(hThetaX(i,:));
+%     term2 = (ones(1,num_labels) - yVec(i,:)) .* log(ones(1,num_labels) - hThetaX(i,:));
+%     J = J + sum(term1 - term2);
+%     
+% end
+% 
+% J = J / m;
+
+J = 1/m * sum(sum(-1 * yVec .* log(hThetaX)-(1-yVec) .* log(1-hThetaX)));
+
+regularator = (sum(sum(Theta1(:,2:end).^2)) + sum(sum(Theta2(:,2:end).^2))) * (lambda/(2*m));
+
+J = J + regularator;
+
+%% Part 2 implementation
 
 
-% Compute "ACCUMULATORS" (coeff for Gradients)
-D1 = d2(:,2:end)' * a1;    % coeff for Theta1_grad (bias term has no error)
-D2 = d3' * a2;             % coeff for Theta2_grad (y' has no bias term)
+
+for t = 1:m
+
+	% For the input layer, where l=1:
+	a1 = [1; X(t,:)'];
+
+	% For the hidden layers, where l=2:
+	z2 = Theta1 * a1;
+	a2 = [1; sigmoid(z2)];
+
+	z3 = Theta2 * a2;
+	a3 = sigmoid(z3);
+
+	yy = ([1:num_labels]==y(t))';
+	% For the delta values:
+	delta_3 = a3 - yy;
+
+	delta_2 = (Theta2' * delta_3) .* [1; sigmoidGradient(z2)];
+	delta_2 = delta_2(2:end); % Taking of the bias row
+
+	% delta_1 is not calculated because we do not associate error with the input    
+
+	% Big delta update
+	Theta1_grad = Theta1_grad + delta_2 * a1';
+	Theta2_grad = Theta2_grad + delta_3 * a2';
+end
+
+Theta1_grad = (1/m) * Theta1_grad + (lambda/m) * [zeros(size(Theta1, 1), 1) Theta1(:,2:end)];
+Theta2_grad = (1/m) * Theta2_grad + (lambda/m) * [zeros(size(Theta2, 1), 1) Theta2(:,2:end)];
 
 
-% Compute Gradient
-Theta1_grad = (1/m)*D1;
-Theta2_grad = (1/m)*D2;
 
-%Add regularization-term to non-bias units
-Theta1_grad(:,2:end) = Theta1_grad(:,2:end) + (lambda/m)*(Theta1(:,2:end));
-Theta2_grad(:,2:end) = Theta2_grad(:,2:end) +  (lambda/m)*(Theta2(:,2:end));
+% -------------------------------------------------------------
 
-Theta1_grad = Theta1_grad;
-Theta2_grad = Theta2_grad;
+% =========================================================================
 
+% Unroll gradients
 grad = [Theta1_grad(:) ; Theta2_grad(:)];
+
+
 end
